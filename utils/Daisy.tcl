50563a64e7a85b39b516a56ce9ea1b36c9ec9d94
abraham <abraham@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2001-11-09 15:20:16 +0000

    sync
    git-svn-id: https://daisy-model.googlecode.com/svn/trunk@746 5f5bf4bb-3343-0410-8278-4fa8eb3319dc

---
+++ utils/Daisy.tcl
@@ -0,0 +1,79 @@
+!TCL=6500, Sander Bruun
+!TITLE=Daisy
+!SORT=N
+
+!TEXT=Daisy
+(activate output activate output)
+(column column ...)
+(description description)
+(harvest harvest ...)
+(input input)
+(manager manager)
+(output output ...)
+(time time)
+(weather weather)
+
+
+!
+!TEXT=om
+(C C ...)
+(C_per_N C_per_N ...)
+(efficiency efficiency ...)
+(fractions fractions ...)
+(initial C_per_N_initial C_per_N)
+(initial_fraction initial_fraction)
+(maintenance maintenance)
+(top_C top_C)
+(top_N top_N)
+(turnover_rate turnover_rate)
+
+
+!
+!TEXT=OrganicMatter
+(Bioincorporation Bioincorporation)
+(K_NH4 K_NH4)
+(K_NO3 K_NO3)
+(active_groundwater active_groundwater)
+(active_underground active_underground)
+(am am ...)
+(buffer_buffer)
+(clay_factor clay_factor)
+(heat_factor heat_factor)
+(initial_SOM initial_SOM ...)
+(min_AM_C min_AM_C)
+(min_AM_N min_AM_N)
+(smb smb ...)
+(smb_tillage_factor smb_tillage_factor ...)
+(som som ...)
+(som_tillage_factor som_tillage_factor ...)
+(tillage_age tillage_age ...)
+(water_factor water_factor)
+
+
+
+!
+!TEXT=Crop
+(fertilize_at fertilize_at ...)
+(fertilize_at_index fertilize_at_index)
+(fertilize_incorporate fertilize _incorporate)
+(harvest_annual harvest_annual)
+(harvest_perennial harvest_perennial)
+(irrigation irrigation)
+(irrigation_delay irrigation_delay)
+(irrigation_rest irrigation_rest)
+(irrigation_year irrigation_year)
+(primary primary)
+(secondary secondary)
+(spray spray ...)
+(spray_index spray_index)
+(tillage tillage ...)
+(tillage_index tillage_index)
+
+!
+!TEXT=Macropore
+default (distribution distribution)
+(height_end height end)
+(height_start height start)
+(pond_max pond max)
+(pressure_end pressure end)
+(pressure_initiate pressure initiate)
