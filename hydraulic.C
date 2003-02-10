c7c1f4556115d627c6ac692fdf8c438a38b5f26b
abraham <abraham@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2003-02-10 15:16:01 +0000

    bcc warning
    git-svn-id: https://daisy-model.googlecode.com/svn/trunk@961 5f5bf4bb-3343-0410-8278-4fa8eb3319dc

--- hydraulic.C
+++ hydraulic.C
@@ -61,7 +61,7 @@ Hydraulic::K_to_M (PLF& plf, const int intervals) const
 	      tmp () << "Hydraulic conductivity changes too fast in " 
 		     << name << "\n";
 	      tmp () << "h = " << h << ", step = " << step 
-		     << " and h + step = " << h + step << "\n";
+		     << " and h + step = " << (h + step) << "\n";
 	      tmp () << "K (h) = " << K (h) << ", K (h + step) = "
 		     << K (h + step) << " and K (0) = " << Ksat << "\n";
 	      tmp () << "Change = " << K (h + step) / K (h) 
