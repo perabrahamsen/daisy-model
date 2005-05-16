b796d8a7141cb90fab73abf59d201fa419d1b5e0
abraham <abraham@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2005-05-16 19:25:14 +0000

    Version 3.73
    git-svn-id: https://daisy-model.googlecode.com/svn/trunk@1406 5f5bf4bb-3343-0410-8278-4fa8eb3319dc

--- version.C
+++ version.C
@@ -1,4 +1,4 @@
 // version.C -- automatically generated file
  
-extern const char *const version = "3.72";
+extern const char *const version = "3.73";
 extern const char *const version_date = __DATE__;
