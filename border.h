3202381f8da2316b343334afa6635941bd5c1e10
abraham <abraham@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2005-06-20 11:28:52 +0000

    x40
    git-svn-id: https://daisy-model.googlecode.com/svn/trunk@1414 5f5bf4bb-3343-0410-8278-4fa8eb3319dc

---
+++ border.h
@@ -0,0 +1,30 @@
+// border.h -- Saving information about the simulation.
+// 
+// Copyright 2005 Per Abrahamsen and KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#ifndef BORDER_H
+#define BORDER_H
+
+class Treelog;
+
+struct Border
+{ virtual bool check_border (double, Treelog&) const = 0; };
+
+#endif // BORDER_H
