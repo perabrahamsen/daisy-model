38c1f42a9c8b32a332b45c4b18bcbaa72abb9b76
abraham <abraham@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2006-01-02 19:47:01 +0000

    x40
    git-svn-id: https://daisy-model.googlecode.com/svn/trunk@1664 5f5bf4bb-3343-0410-8278-4fa8eb3319dc

--- select_content.C
+++ select_content.C
@@ -23,21 +23,80 @@
 #include "select_value.h"
 #include "soil.h"
 #include "check.h"
+#include "mathlib.h"
 
 struct SelectContent : public SelectValue
 {
   // Content.
   const double height;
+  const Soil* old_soil;
+  int ia;                       // First node above height.
+  double za;                    // Depth of node above height.
+  int ib;                       // First node below height.
+  double zb;                    // Depth of node above height.
+  double rel;                   // Relative distance from za.
+  bool use_ia;                  // Always use ia.
 
   // Output routines.
   void output_array (const std::vector<double>& array, 
 		     const Soil* soil, Treelog&)
-  { add_result (array[soil->interval_plus (height)]); }
+  { 
+    if (soil != old_soil)
+      {
+        old_soil = soil;
+
+        if (std::fabs (height) < 0.001)
+          ia = ib = 0;
+        else
+          {
+            // Find first node below height.
+            for (ib = 0; ib < soil->size () && soil->z (ib) > height; ib++)
+              /* do nothing */;
+            zb = soil->z (ib);
+        
+            // Find first node above height.
+            daisy_assert (soil->size () > 0);
+            for (ia = soil->size () - 1; ia >= 0 && soil->z (ia) < height; ia--)
+              /* do nothing */;
+            za = soil->z (ia);
+
+            // Distance from za relative to zb.
+            rel = (height - za) / (zb - za) ;
+
+            // If one is almost right, use that.
+            if (approximate (height, za))
+              ib = ia;
+            else if (approximate (height, zb))
+              ia = ib;
+          }
+        
+        // If they are the same, just use ia. 
+        use_ia = (ia == ib);
+      }
+    double result;
+    if (use_ia) 
+      result = array [ia];
+    else
+      {
+        // Linear interpolation.
+        const double va = array[ia];
+        const double vb = array[ib];
+        result = va + (vb - va) * rel;
+      }
+    add_result (result); 
+  }
 
   // Create and Destroy.
   SelectContent (Block& al)
     : SelectValue (al),
-      height (al.number ("height"))
+      height (al.number ("height")),
+      old_soil (NULL),
+      ia (-42),
+      za (42.42e42),
+      ib (-42),
+      zb (42.42e42),
+      rel (42.42e42),
+      use_ia (true)
     { }
 };
 
