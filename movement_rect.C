55ed3f033fa11b79e8dcd0b204cba0adcb7c3c5f
per.abrahamsen <per.abrahamsen@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2008-05-26 14:28:40 +0000

    kvl-t
    git-svn-id: https://daisy-model.googlecode.com/svn/trunk@2502 5f5bf4bb-3343-0410-8278-4fa8eb3319dc

--- movement_rect.C
+++ movement_rect.C
@@ -254,7 +254,8 @@ MovementRect::tick (const Soil& soil, SoilWater& soil_water,
       if (geo->edge_to (edge) == Geometry::cell_above)
 	{
 	  if (obey_surface)
-	    surface.accept_top (soil_water.q_matrix (edge) * dt, 
+	    surface.accept_top ((soil_water.q_matrix (edge) 
+                                 + soil_water.q_tertiary (edge)) * dt, 
                                 *geo, edge, dt, msg);
 	  else
 	    surface.accept_top (surface.q_top (*geo, edge),
