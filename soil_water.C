fc9bfb4c4b104a24c2208a5ac3abb302807b34dd
per.abrahamsen <per.abrahamsen@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2008-09-26 09:04:32 +0000

    x61s
    git-svn-id: https://daisy-model.googlecode.com/svn/trunk@2839 5f5bf4bb-3343-0410-8278-4fa8eb3319dc

--- soil_water.C
+++ soil_water.C
@@ -65,7 +65,7 @@ SoilWater::drain (const std::vector<double>& v)
   daisy_assert (S_root_.size () == v.size ());
   for (unsigned i = 0; i < v.size (); i++)
     {
-      if (v[i] < 1e-9)
+      if (v[i] < -1e-9)
         {
           std::ostringstream tmp;
           tmp << "draining " << v[i] << " [h^-1] from cell " << i;
