61146a8982ba95ac8768a94bb89551fdd6c2021b
per.abrahamsen <per.abrahamsen@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2010-04-09 09:25:14 +0000

    Version 4.84
    git-svn-id: https://daisy-model.googlecode.com/svn/trunk@3560 5f5bf4bb-3343-0410-8278-4fa8eb3319dc

--- version.C
+++ version.C
@@ -1,4 +1,4 @@
 // version.C -- automatically generated file
  
-extern const char *const version = "4.83";
+extern const char *const version = "4.84";
 extern const char *const version_date = __DATE__;
