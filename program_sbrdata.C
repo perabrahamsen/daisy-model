7edbc64fa0fb9d14906cd382fec9a827dab9a371
per.abrahamsen <per.abrahamsen@5f5bf4bb-3343-0410-8278-4fa8eb3319dc>
2015-03-20 15:09:06 +0000

    make
    git-svn-id: https://daisy-model.googlecode.com/svn/trunk@4259 5f5bf4bb-3343-0410-8278-4fa8eb3319dc

---
+++ program_sbrdata.C
@@ -0,0 +1,386 @@
+// program_sbrdata.C -- Read Agrovand data.
+// 
+// Copyright 1996-2001 Per Abrahamsen and Søren Hansen
+// Copyright 2000-2001 KVL.
+// Copyright 2007, 2008, 2009 Per Abrahamsen and KVL.
+//
+// This file is part of Daisy.
+// 
+// Daisy is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+// 
+// Daisy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser Public License
+// along with Daisy; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#define BUILD_DLL
+
+#include "program.h"
+#include "lexer_data.h"
+#include "librarian.h"
+#include "assertion.h"
+#include "time.h"
+#include "treelog.h"
+#include "block_model.h"
+#include "path.h"
+#include <fstream>
+#include <sstream>
+
+struct ProgramRS2WG : public Program
+{
+  const Path& path;
+  const Time rshourly_origin;
+  const Time wgcycle_begin;
+  const Time wgcycle_end;
+  const symbol rshourly_file;
+  const symbol rsdaily_file;
+  const symbol dwfhourly_file;
+
+  // Use.
+  bool run (Treelog& msg)
+  {
+    // Open files.
+    std::auto_ptr<std::istream> rsin = path.open_file (rshourly_file);
+    daisy_assert (rsin.get ());
+    LexerData rs_hourly (rshourly_file, *(rsin.get ()), msg);
+    if (!rs_hourly.good ())
+      {
+        msg.error (std::string ("Problems opening '") + rshourly_file + "'");
+        return false;
+      }
+  
+    std::ofstream rs_daily (rsdaily_file.name ().c_str ());
+    if (!rs_daily.good ())
+      {
+        msg.error (std::string ("Problems opening '") 
+                   + rsdaily_file + "' for writing");
+        return false;
+      }
+
+    std::ofstream dwf_hourly (dwfhourly_file.name ().c_str ());
+    if (!dwf_hourly.good ())
+      {
+        msg.error (std::string ("Problems opening '")
+                   + dwfhourly_file + "' for writing");
+        return false;
+      }
+      
+    dwf_hourly << "\
+dwf-0.0 -- " << rshourly_file << "\n\
+--------\n\
+Date	Precip\n\
+date	mm\n";
+    Time time = rshourly_origin;
+    Time cycle_time = wgcycle_begin;
+
+    double daily = 0.0;
+    do
+      {
+        const double value = rs_hourly.get_number ();
+        if (!rs_hourly.good ())
+          break;
+        dwf_hourly << time.print () << "\t" << value << "\n";
+        daily += value;
+        Time next = time;
+        next.tick_hour (1);
+        if (next.mday () != time.mday ()) 
+          // New day.
+          {
+            cycle_time.tick_day ();
+            if (cycle_time == wgcycle_end)
+              // New cycle.
+              cycle_time = wgcycle_begin;
+
+            if (cycle_time.mday () == next.mday ())
+              // We agree on the time.
+              rs_daily << daily << "\n";
+            else if (cycle_time.mday () == 29)
+              {
+                // Generate extra leap day.
+                daisy_assert (next.mday () == 1);
+                rs_daily << daily << "\n";
+                rs_daily << daily << "\n";
+                cycle_time.tick_day ();
+              }
+            else if (next.mday () == 29)
+              {
+                // Skip leap day.
+                daisy_assert (cycle_time.mday () == 1);
+                cycle_time.tick_day (-1);
+
+              }
+            else
+              // We should agree all other days.
+              {
+                msg.error ("RS = " + next.print () 
+                           + ", WG = " + cycle_time.print ());
+                daisy_notreached ();
+              }
+
+            daily = 0.0;
+          }
+        if (time.year () / 100 != next.year () / 100)
+          msg.message (next.print ());
+        time = next;
+        rs_hourly.next_line ();
+      }
+    while (rs_hourly.good ());
+
+    if (!rs_daily.good ())
+      {
+        msg.error (std::string ("Problems writing to '") 
+                   + rsdaily_file + "'");
+      }
+
+    if (!dwf_hourly.good ())
+      {
+        msg.error (std::string ("Problems writing to '")
+                   + dwfhourly_file + "'");
+      }
+
+    return true;
+  }
+
+  // Create and Destroy.
+  void initialize (Block&)
+  { }
+
+  bool check (Treelog& msg)
+  {
+    bool ok = true;
+    return ok; 
+  }
+
+  ProgramRS2WG (const BlockModel& al)
+    : Program (al),
+      path (al.path ()),
+      rshourly_origin (al.integer ("rshourly_origin"), 1, 1, 0),
+      wgcycle_begin (al.integer ("wgcycle_begin"), 1, 1, 0),
+      wgcycle_end (al.integer ("wgcycle_begin") + al.integer ("wgcycle_length"),
+                   1, 1, 0),
+      rshourly_file (al.name ("rshourly_file")),
+      rsdaily_file (al.name ("rsdaily_file")),
+      dwfhourly_file (al.name ("dwfhourly_file"))
+  { }
+  ~ProgramRS2WG ()
+  { }
+};
+
+static struct ProgramRS2WGSyntax : public DeclareModel
+{
+  Model* make (const BlockModel& al) const
+  { return new ProgramRS2WG (al); }
+  ProgramRS2WGSyntax ()
+    : DeclareModel (Program::component, "RS2WG", "\
+Prepare data from RainSim to the weather generator.")
+  { }
+  void load_frame (Frame& frame) const
+  { 
+    frame.declare_integer ("rshourly_origin", Attribute::Const, "\
+Start of hourly data from RainSim.");
+    frame.declare_integer ("wgcycle_begin", Attribute::Const, "\
+Start of weather cycle for Weather Generator.");
+    frame.declare_integer ("wgcycle_length", Attribute::Const, "\
+Length (in years) of weather cycle for Weather Generator.");
+    frame.declare_string ("rshourly_file", Attribute::Const, "\
+Name of file with hourly data from RainSim.");
+    frame.declare_string ("rsdaily_file", Attribute::Const, "\
+Name of file with daily precipitation for the Weather Generator.");
+    frame.declare_string ("dwfhourly_file", Attribute::Const, "\
+Daisy weather file with hourly data from RainSim.");
+  }
+} ProgramRS2WG_syntax;
+
+// Weather generator to Daisy
+
+struct ProgramWG2DWF : public Program
+{
+  const Path& path;
+  const Time rshourly_origin;
+  const Time wgcycle_begin;
+  const Time wgcycle_end;
+  const symbol wg_file;
+  const symbol dwfdaily_file;
+  const symbol header;
+
+  // Use.
+  bool run (Treelog& msg)
+  {
+    // Open files.
+    std::auto_ptr<std::istream> wgin = path.open_file (wg_file);
+    daisy_assert (wgin.get ());
+    LexerData wg (wg_file, *(wgin.get ()), msg);
+    if (!wg.good ())
+      {
+        msg.error (std::string ("Problems opening '") + wg_file + "'");
+        return false;
+      }
+    wg.skip_line ();
+    wg.next_line ();
+  
+    std::ofstream dwf_daily (dwfdaily_file.name ().c_str ());
+    if (!dwf_daily.good ())
+      {
+        msg.error (std::string ("Problems opening '")
+                   + dwfdaily_file + "' for writing");
+        return false;
+      }
+      
+    dwf_daily << "\
+dwf-0.0 -- " << wg_file << "\n" << header << "\n----\n\
+Year	Month	Day	Precip	T_min	T_max	VapPres	Wind	DiffRad	GlobRad	RefEvap\n\
+year	month	day	mm/d	dgC	dgC	Pa	m/s	W/m^2	W/m^2	mm/d\n";
+
+    Time time = rshourly_origin;
+    Time cycle_time = wgcycle_begin;
+
+    while (wg.good ())
+      {
+        (void) wg.get_cardinal ();
+        wg.skip_space ();
+        const int year = wg.get_cardinal ();
+        wg.skip_space ();
+        const int month = wg.get_cardinal ();
+        wg.skip_space ();
+        const int mday = wg.get_cardinal ();
+        wg.skip_space ();
+        (void) wg.get_cardinal ();
+        wg.skip_space ();
+        (void) wg.get_cardinal ();
+        wg.skip_space ();
+        const double Precip = wg.get_number ();
+        wg.skip_space ();
+        const double Tmin = wg.get_number ();
+        wg.skip_space ();
+        const double Tmax = wg.get_number ();
+        wg.skip_space ();
+        const double Vp = wg.get_number ();
+        wg.skip_space ();
+        const double rh = wg.get_number ();
+        wg.skip_space ();
+        const double wind = wg.get_number ();
+        wg.skip_space ();
+        const double ss = wg.get_number ();
+        wg.skip_space ();
+        const double diffrad = wg.get_number ();
+        wg.skip_space ();
+        const double direct = wg.get_number ();
+        wg.skip_space ();
+        const double pet = wg.get_number ();
+        wg.next_line ();
+
+        daisy_assert (year == cycle_time.year ());
+        daisy_assert (month == cycle_time.month ());
+        daisy_assert (mday == cycle_time.mday ());
+
+        std::ostringstream tmp;
+        tmp << Precip << "\t" << Tmin << "\t" << Tmax << "\t" << Vp
+            << "\t" << wind << "\t" << diffrad << "\t" << (diffrad + direct)
+            << "\t" << pet << "\n"; 
+        const std::string daily = tmp.str ();
+
+        if (cycle_time.mday () == time.mday ())
+          // We agree on the time.
+          dwf_daily << time.year () << "\t" << time.month () 
+                    << "\t" << time.mday () << "\t" << daily;
+        else if (cycle_time.mday () == 29)
+          {
+            // Skip leap day.
+            daisy_assert (time.mday () == 1);
+            time.tick_day (-1);
+          }
+        else if (time.mday () == 29)
+          {
+            // Generate extra leap day.
+            daisy_assert (cycle_time.mday () == 1);
+            dwf_daily << time.year () << "\t" << time.month () 
+                      << "\t" << time.mday () << "\t" << daily;
+            time.tick_day ();
+            dwf_daily << time.year () << "\t" << time.month () 
+                    << "\t" << time.mday () << "\t" << daily;
+          }
+        else
+          // We should agree all other days.
+          {
+            msg.error ("RS = " + time.print () 
+                       + ", WG = " + cycle_time.print ());
+            daisy_notreached ();
+          }
+
+        // Next day.
+        time.tick_day ();
+        cycle_time.tick_day ();
+        if (cycle_time == wgcycle_end)
+          cycle_time = wgcycle_begin;
+
+
+
+      }
+
+    if (!dwf_daily.good ())
+      {
+        msg.error (std::string ("Problems writing to '")
+                   + dwfdaily_file + "'");
+      }
+
+    return true;
+  }
+
+  // Create and Destroy.
+  void initialize (Block&)
+  { }
+
+  bool check (Treelog& msg)
+  {
+    bool ok = true;
+    return ok; 
+  }
+
+  ProgramWG2DWF (const BlockModel& al)
+    : Program (al),
+      path (al.path ()),
+      rshourly_origin (al.integer ("rshourly_origin"), 1, 1, 0),
+      wgcycle_begin (al.integer ("wgcycle_begin"), 1, 1, 0),
+      wgcycle_end (al.integer ("wgcycle_begin") + al.integer ("wgcycle_length"),
+                   1, 1, 0),
+      wg_file (al.name ("wg_file")),
+      dwfdaily_file (al.name ("dwfdaily_file")),
+      header (al.name ("header"))
+  { }
+  ~ProgramWG2DWF ()
+  { }
+};
+
+static struct ProgramWG2DWFSyntax : public DeclareModel
+{
+  Model* make (const BlockModel& al) const
+  { return new ProgramWG2DWF (al); }
+  ProgramWG2DWFSyntax ()
+    : DeclareModel (Program::component, "WG2DWF", "\
+Prepare data from the weather generator for Daisy.")
+  { }
+  void load_frame (Frame& frame) const
+  { 
+    frame.declare_integer ("rshourly_origin", Attribute::Const, "\
+Start of hourly data from RainSim.");
+    frame.declare_integer ("wgcycle_begin", Attribute::Const, "\
+Start of weather cycle for Weather Generator.");
+    frame.declare_integer ("wgcycle_length", Attribute::Const, "\
+Length (in years) of weather cycle for Weather Generator.");
+    frame.declare_string ("wg_file", Attribute::Const, "\
+Name of file generated by the Weather Generator.");
+    frame.declare_string ("dwfdaily_file", Attribute::Const, "\
+Daisy weather file with daily data from Weather Generator.");
+    frame.declare_string ("header", Attribute::Const, "\
+Stuff to put in the Daisy weather file header.");
+  }
+} ProgramWG2DWF_syntax;
+
+// program_sbrdata.C ends here.
