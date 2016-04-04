c2335caf9ca082623d368b66300e376a0a9e7676
Per Abrahamsen <per.abrahamsen@gmail.com>
2016-04-04 14:58:39 +0200

    Version 5.25

--- version.C
+++ version.C
@@ -1,4 +1,4 @@
 // version.C -- automatically generated file
  
-extern const char *const version = "5.24";
+extern const char *const version = "5.25";
 extern const char *const version_date = __DATE__;
