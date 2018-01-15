7870ace55c0d71fab5295da4093a546c7c4ed0a2
Per Abrahamsen <per.abrahamsen@gmail.com>
2018-01-15 17:53:16 +0100

    v53

--- version.C
+++ version.C
@@ -1,4 +1,4 @@
 // version.C -- automatically generated file
  
-extern const char *const version = "5.52";
+extern const char *const version = "5.53";
 extern const char *const version_date = __DATE__;
