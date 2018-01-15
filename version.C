1f4a5cca985ce2f6a56baca6cbe639698e7c2de7
Per Abrahamsen <per.abrahamsen@gmail.com>
2018-01-15 13:45:46 +0100

    new FAO_PM model for testing, fix

--- version.C
+++ version.C
@@ -1,4 +1,4 @@
 // version.C -- automatically generated file
  
-extern const char *const version = "5.51";
+extern const char *const version = "5.52";
 extern const char *const version_date = __DATE__;
