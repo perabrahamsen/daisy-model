85b8c0b4d095ad8840123072ee0392bcc014e911
Per Abrahamsen <per.abrahamsen@gmail.com>
2020-10-12 15:37:05 +0200

    Version 5.94

--- version.C
+++ version.C
@@ -1,4 +1,4 @@
 // version.C -- automatically generated file
  
-extern const char *const version = "5.93";
+extern const char *const version = "5.94";
 extern const char *const version_date = __DATE__;
