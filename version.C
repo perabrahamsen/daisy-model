3a5d69c9e7b18875f4e566f8c4d85f40d8fe26e6
Per Abrahamsen <per.abrahamsen@gmail.com>
2021-06-01 13:27:19 +0200

    Version 6.15

--- version.C
+++ version.C
@@ -1,4 +1,4 @@
 // version.C -- automatically generated file
  
-extern const char *const version = "6.14";
+extern const char *const version = "6.15";
 extern const char *const version_date = __DATE__;
