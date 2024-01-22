3128b1dba79a24371c6aeebcf935d5699199c4d1
Per Abrahamsen <pa@plen.ku.dk>
2024-01-22 14:31:57 +0100

    Version 6.45

--- version.C
+++ version.C
@@ -1,4 +1,4 @@
 // version.C -- automatically generated file
  
-extern const char *const version = "6.44";
+extern const char *const version = "6.45";
 extern const char *const version_date = __DATE__;
